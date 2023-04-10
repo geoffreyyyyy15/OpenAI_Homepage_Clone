6144dfd3ad9ddb7919a26b83e04271e54c3d9a57
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-10 08:20:49 +0800

    Responsive design for grids on mobile device

--- resources/views/components/grid.blade.php
+++ resources/views/components/grid.blade.php
@@ -1,6 +1,6 @@
 @props(['column'])
 @php
-    $classes = "grid grid-cols-$column  gap-10   text-white "
+    $classes = "grid grid-cols-$column gap-4 text-white xs:block xl:inline-flex lg:inline-flex"
 @endphp
 <div {{ $attributes->merge(['class' => $classes ]) }}>
     {{ $slot }}
