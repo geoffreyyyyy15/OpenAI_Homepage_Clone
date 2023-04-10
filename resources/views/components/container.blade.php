d7d3aee5627e7e04835506b05e097788f7897fce
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-10 08:19:53 +0800

    Added title per section

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,3 +1,8 @@
-<div class="justify-center items-center bg-black p-10 m-5">
+@props(['title'])
+@php
+$classes = "flex sm:block bg-black p-10 m-5"    
+@endphp
+<h1 class="text-white">Research</h1>
+<div {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </div>
