147b8c2a6af9ab5ed85d0305061ccad3b3abe859
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-10 08:25:08 +0800

    custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,17 @@ module.exports = {
         "./resources/**/*.vue",
       ],
   theme: {
+    screens: {
+      'xs': '410px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
+  variants: {},
   plugins: [],
 }
 
