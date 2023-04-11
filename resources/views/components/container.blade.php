d64956cf563f73d48fa5793ffe38af092d260aa6
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-04-11 11:51:28 +0800

    put title on every section

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,8 +1,8 @@
 @props(['title'])
 @php
-$classes = "flex sm:block bg-black p-10 m-5"    
+$classes = "flex sm:block bg-black p-10 m-5"
 @endphp
-<h1 class="text-white">Research</h1>
+<h1 class="text-white p-2 m-5" style="font-size: 40px">{{ $title }}</h1>
 <div {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </div>
