79987f968c7ecfce68c6d3189cf9a71a154218b5
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-04-12 11:32:21 +0800

    Update underline-word.blade.php

--- resources/views/components/underline-word.blade.php
+++ resources/views/components/underline-word.blade.php
@@ -1,7 +1,7 @@
 @props(['text'])
 @php
-    $classes = "f-ui-1 underline-thickness-1 underline-offset-4 underline  group-hover:decoration-[rgba(var(--text-primary-rgb),0.2)]"
+    $classes = "f-ui-1 underline-thickness-1 underline underline-offset-4 hover:decoration-gray-800 group-hover:decoration-[rgba(var(--text-primary-rgb),0.2)]"
 @endphp
-<a href="#" class="text-white font-bold">
-    <span {{ $attributes->merge(['class' => $classes]) }}>{{ $text }}</span>
-</a>
+
+    <span {{ $attributes->merge(['class' => $classes]) }}><a href="#" class="text-white font-bold">{{ $text }}</a></span>
+
