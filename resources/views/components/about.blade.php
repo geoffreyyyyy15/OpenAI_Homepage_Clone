45babf59c50e40cd81b9747819fcdfb34ca5a134
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-04-12 11:30:50 +0800

    adding hover underline in buttons

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -6,6 +6,6 @@
     <img src="{{ $path }}" alt="img" style="width:500px; height: 300px" {{$hidden}}>
     <h2 class="font-bold text-white text-xl mb-3">{{ $title }}</h2>
     <a href="#" class="relative">
-        <span class="f-ui-1 underline-thickness-1 underline-offset-4 underline  group-hover:decoration-[rgba(var(--text-primary-rgb),0.2)]">{{ $excerpt }}</span>
+        <span class="f-ui-1 underline-thickness-1 underline underline-offset-4 hover:decoration-gray-800 group-hover:decoration-[rgba(var(--text-primary-rgb),0.2)]">{{ $excerpt }}</span>
     </a>
 </div>
