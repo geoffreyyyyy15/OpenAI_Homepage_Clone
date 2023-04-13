78743fda5dbe9170c0dd4d1fa99501638ddf7551
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-04-13 10:48:15 +0800

    added file in form register

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -3,12 +3,13 @@
         <div class="flex justify-center items-center w-full h-screen relative">
             <div class="bg-gray-900 shadow-xl shadow-gray-400 rounded-lg p-5 mb-5 mx-auto">
                 <div class="flex flex-col gap-1 p-3">
-                    <form action="{{ route('user.create') }}" method="post" class="flex flex-col gap-2">
+                    <form action="{{ route('user.create') }}" method="post" class="flex flex-col gap-2" enctype="multipart/form-data">
                         @csrf
-                       <x-form name="email" type="email" />
-                       <x-form name="name" type="text" />
-                       <x-form name="username" type="text" />
-                       <x-form name="password" type="password" />
+                        <x-form name="email" type="email" />
+                        <x-form name="name" type="text" />
+                        <x-form name="picture" type="file" />
+                        <x-form name="username" type="text" />
+                        <x-form name="password" type="password" />
 
                         <div class="flex justify-end relative gap-2 mt-2">
                             <a href="{{ route('login') }}" class="underline text-gray-600 mt-1">Already have an account?</a>
