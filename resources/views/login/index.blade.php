4454cd760bcdab3ed441329a67104dec710b5053
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-30 16:24:49 +0800

    fix login form

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,16 +1,18 @@
 <x-layout >
-    <div class="flex justify-center ">
-        <div class=" bg-gray-900 shadow-xl shadow-gray-500 mb-10 p-2 w-96 h-96  mt-20 rounded-lg justify-center items-center">
-            <div class="flex-auto w-32 m-5 justify-center items-center relative">
+    <div class="flex justify-center items-center h-screen w-full relative">
+        <div class=" bg-gray-900 shadow-xl shadow-gray-400 rounded-lg p-5 mb-5 mx-auto">
+            <div class="flex flex-col gap-1 p-3">
                 <form action="/sessions" method="post" class="">
-                    <h1 class="text-lg text-white">Login</h1>
+                    <div class="flex items-center justify-center">
+                        <h1 class="text-xl text-white">Login</h1>
+                    </div>
                     @csrf
                     <div class="flex flex-col gap-2 justify-center p-2 ">
-                        <a href="{{route('register')}}" class="text-white">Click here to Sign-up</a>
+                        <a href="{{route('register')}}" class="text-white">Click <span class="text-blue-500">here</span> to Sign-up</a>
                         <x-form name="email" type="email"/>
                         <x-form name="password" type="password"/>
-                        <div class="flex mt-2">
-                            <x-form.button>Submit </x-form.button>
+                        <div class="flex mt-2 items-center justify-center ">
+                            <x-form.button class="hover:-translate-y-1 duration-300">Submit </x-form.button>
                         </div>
                     </div>
                 </form>
