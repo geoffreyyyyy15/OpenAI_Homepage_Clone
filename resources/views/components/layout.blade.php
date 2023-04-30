52dc9de9dcc7b3eb3f06b4e6d3753a9d8f714354
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-30 16:24:36 +0800

    fix profile picture on navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -101,7 +101,7 @@
                 @auth
                 <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8  rounded-full" src="{{asset("storage/" . auth()->user()->picture)}}" alt="user photo">
+                    <img class="w-8 h-8  rounded-full" src="{{ asset("storage/" . auth()->user()->picture) }}" alt="user photo">
                 </button>
                 @endauth
                 <!-- Dropdown menu -->
