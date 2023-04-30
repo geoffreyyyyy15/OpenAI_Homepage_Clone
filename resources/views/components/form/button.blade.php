a98135a9ac087707230dab5c84017b4f4492b4f8
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-04-30 16:24:16 +0800

    adding merge class in submit button component

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,1 +1,4 @@
-<button class="bg-white p-2 rounded-lg " type="submit">{{ $slot }}</button>
+@php
+    $classes = "bg-white p-2 rounded-lg transition ease-in-out hover:scale-110 hover:bg-gray-300"
+@endphp
+<button {{ $attributes->merge(['class' => $classes]) }} type="submit">{{ $slot }}</button>
